6f6bf261d0557154e232845d5a0c9c94945d816d
Anastasia Tomilina <tomilina.backend@gmail.com>
2020-07-06 17:05:00 +0300

    Create 2.3-Type-classes.hs

---
+++ 2.3-Type-classes.hs
@@ -0,0 +1,14 @@
+module TypeClasses where
+
+class Printable a where
+  toString :: a -> String
+
+instance Printable Bool where
+  toString True = "true"
+  toString False = "false"
+
+instance Printable () where
+  toString () = "unit type"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+  toString (a, b) = "(" ++ (toString a) ++ "," ++ (toString b) ++ ")"
