b596b085ae23941f9fccb2d31d6be52e5d5e05f1
Anastasia Tomilina <tomilina.backend@gmail.com>
2020-07-13 22:54:56 +0300

    Update 3.2-ListFuncs.hs

--- 3.2-ListFuncs.hs
+++ 3.2-ListFuncs.hs
@@ -9,3 +9,16 @@ readDigits = span (isDigit)
 filterDisj :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
 filterDisj p1 p2 xs = filter (pred p1 p2) xs
   where pred x y cur = x cur || y cur
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort [x] = [x]
+qsort xs = qsort(filter (< head xs) xs) ++ filter (== head xs) xs ++ qsort(filter (> head xs) xs)
+
+squares'n'cubes :: Num a => [a] -> [a]
+squares'n'cubes = concatMap (\x -> [x^2, x^3])
+
+perms :: Eq a => [a] -> [[a]]
+perms [] = [[]]
+perms [x] = [[x]]
+perms xs = concatMap (\x -> map (x:) (perms (filter (/=x) xs))) xs
