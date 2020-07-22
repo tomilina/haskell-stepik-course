2cca3d2222dbf8bbac4ad62faf23d014898e8efe
Anastasia Tomilina <tomilina.backend@gmail.com>
2020-07-22 23:12:21 +0300

    Update 2.4-SafeEnum.hs

--- 2.4-SafeEnum.hs
+++ 2.4-SafeEnum.hs
@@ -6,3 +6,12 @@ class (Enum a, Bounded a, Eq a) => SafeEnum a where
 
   spred :: a -> a
   spred a = if (a == minBound) then maxBound else pred a
+
+data Odd = Odd Integer deriving (Eq,Show)
+instance Enum Odd where
+    toEnum :: Int -> Odd
+    toEnum m | m `mod` 2 == 0 = Odd (toInteger(m))
+             | otherwise = error "toEnum: parameter cannot be odd"
+
+    fromEnum :: Odd -> Int
+    fromEnum x = fromIntegral(x)
